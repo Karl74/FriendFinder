32884b359141d8e2451bbbd75e686f2eebb50395
Carlos Castro <carlos.castro74@gmail.com>
2017-04-22 22:21:37 -0500

    front end javascrtipt

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 
